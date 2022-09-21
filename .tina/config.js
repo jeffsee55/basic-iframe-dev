bdea89ee7868b5844b682e66433b5fb351adc33c
Jeff See <jeffsee.55@gmail.com>
2022-09-21 16:46:08 -0700

    get tina static dev working

--- .tina/config.js
+++ .tina/config.js
@@ -1,39 +1,58 @@
-import { defineStaticConfig } from 'tinacms'
+import {
+  defineStaticConfig,
+  NumberFieldPlugin,
+  TextFieldPlugin,
+} from "tinacms";
+// const defineStaticConfig = (obj) => obj;
+import React from "react";
+// import * as ReactIs from "react-is";
+
+// console.log("isit?", ReactIs.isValidElementType("div"));
 
 export default defineStaticConfig({
   build: {
-    outputFolder: 'tina',
-    publicFolder: 'public',
+    outputFolder: "tina",
+    publicFolder: "public",
   },
   schema: {
     collections: [
       {
-        name: 'test',
-        path: 'content/test',
-        label: 'Test',
+        name: "test",
+        path: "content/test",
+        label: "Test",
         templates: [
           {
-            name: 'tem1',
-            label: 'Template 1',
-            fields: [{ type: 'string', name: 'foo' }],
+            name: "tem1",
+            label: "Template 1",
+            fields: [
+              {
+                type: "string",
+                name: "foo",
+                ui: NumberFieldPlugin,
+              },
+              {
+                type: "string",
+                name: "fooj",
+              },
+            ],
           },
           {
-            name: 'tem2',
-            label: 'Template 2',
-            fields: [{ type: 'string', name: 'bar' }],
+            name: "tem2",
+            label: "Template 2",
+            fields: [{ type: "string", name: "bar" }],
           },
         ],
       },
       {
-        name: 'page',
-        path: 'content/page',
-        label: 'Page',
-        format: 'mdx',
+        name: "page",
+        path: "content/page",
+        label: "Page",
+        format: "mdx",
         fields: [
           {
-            label: 'Title',
-            name: 'Title',
-            type: 'string',
+            label: "Title",
+            name: "Title",
+            type: "string",
             ui: {
               // defaultValue: 'Title',
               // Examples of how you COULD use a custom form
@@ -53,114 +72,117 @@ export default defineStaticConfig({
             },
           },
           {
-            name: 'body',
-            label: 'Main Content',
-            type: 'rich-text',
+            name: "body",
+            label: "Main Content",
+            type: "rich-text",
             isBody: true,
           },
         ],
       },
       {
-        label: 'Blog Posts',
-        name: 'post',
-        path: 'content/post',
-        format: 'md',
+        label: "Blog Posts",
+        name: "post",
+        path: "content/post",
+        format: "md",
         ui: {
           router: ({ document }) => {
-            return `/posts/${document._sys.filename}`
+            return `/posts/${document._sys.filename}`;
           },
         },
         fields: [
           {
-            type: 'string',
-            label: 'Title',
-            name: 'title',
+            type: "string",
+            label: "Title",
+            name: "title",
+            ui: {
+              component: () => <div className="bg-blue-600">Well hi</div>,
+            },
           },
           {
-            type: 'object',
-            label: 'Related Posts',
-            name: 'posts',
+            type: "object",
+            label: "Related Posts",
+            name: "posts",
             list: true,
             ui: {
               itemProps: (item) => {
-                return { label: item?.label }
+                return { label: item?.label };
               },
             },
             fields: [
               {
-                name: 'post',
-                type: 'reference',
-                collections: ['post', 'page'],
+                name: "post",
+                type: "reference",
+                collections: ["post", "page"],
               },
               {
-                name: 'label',
-                type: 'string',
+                name: "label",
+                type: "string",
               },
             ],
           },
           {
-            type: 'object',
-            label: 'Something',
-            name: 'foo',
+            type: "object",
+            label: "Something",
+            name: "foo",
             fields: [
               {
-                name: 'bar',
-                label: 'Bar',
-                type: 'string',
+                name: "bar",
+                label: "Bar",
+                type: "string",
               },
             ],
           },
           {
-            type: 'string',
+            type: "string",
 
-            label: 'Topic',
-            name: 'topic',
-            options: ['programming', 'blacksmithing'],
+            label: "Topic",
+            name: "topic",
+            options: ["programming", "blacksmithing"],
             list: true,
           },
           {
-            type: 'rich-text',
-            label: 'Blog Post Body',
-            name: 'body',
+            type: "rich-text",
+            label: "Blog Post Body",
+            name: "body",
             isBody: true,
             templates: [
               {
-                name: 'Gallery',
-                label: 'Gallery',
+                name: "Gallery",
+                label: "Gallery",
                 fields: [
                   {
-                    label: 'Images',
-                    name: 'images',
-                    type: 'object',
+                    label: "Images",
+                    name: "images",
+                    type: "object",
                     list: true,
                     fields: [
                       {
-                        type: 'image',
-                        name: 'src',
-                        label: 'Source',
+                        type: "image",
+                        name: "src",
+                        label: "Source",
                       },
                       {
-                        type: 'string',
-                        name: 'width',
-                        label: 'Width',
+                        type: "string",
+                        name: "width",
+                        label: "Width",
                       },
                       {
-                        type: 'string',
-                        name: 'height',
-                        label: 'Height',
+                        type: "string",
+                        name: "height",
+                        label: "Height",
                       },
                     ],
                   },
                   {
-                    type: 'string',
-                    name: 'alignment',
-                    label: 'Alignment',
-                    options: ['left', 'center', 'right'],
+                    type: "string",
+                    name: "alignment",
+                    label: "Alignment",
+                    options: ["left", "center", "right"],
                   },
                   {
-                    type: 'string',
-                    name: 'gap',
-                    label: 'Gap',
+                    type: "string",
+                    name: "gap",
+                    label: "Gap",
                   },
                 ],
               },
@@ -170,4 +192,4 @@ export default defineStaticConfig({
       },
     ],
   },
-})
+});
